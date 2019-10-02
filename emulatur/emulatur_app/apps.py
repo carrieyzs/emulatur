d9f0b4b30739583d3f5057b02e64fa0ca3dc89f1
Carrie Yang <yangcarr@myvuw.ac.nz>
2019-10-02 17:03:36 +1300

    Remove files.

--- emulatur/emulatur_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmulaturAppConfig(AppConfig):
-    name = 'emulatur_app'
--- emulatur/emulatur_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from django.contrib import admin
-
-from . import views
-
-urlpatterns = [
-    path('emulatur_app/', include('emulatur_app.urls')),
-    path('admin/', admin.site.urls),
-]
