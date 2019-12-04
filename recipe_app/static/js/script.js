df606a11d6fdda25fd5aa4fb03084529004a4081
Carrie Yang <yangcarr@myvuw.ac.nz>
2019-12-04 22:27:57 +1300

    Implemented toggling on beak.

--- recipe_app/static/js/script.js
+++ recipe_app/static/js/script.js
@@ -1,10 +1,18 @@
-function changePic(){
-    console.log("test")
-    var elem = document.createElement("img")
-    elem.setAttribute("width", "60%")
-    elem.setAttribute("height", "auto")
-    elem.setAttribute("margin-top", "-25%")
-    elem.src = "{% static 'images/beak_open.png' %}"
-    document.getElementById("home_beak").appendChild(elem)
+var beakIsOpen = false
 
+function changePic(){
+    console.log(beakIsOpen)
+    var elem = document.createElement("img")
+    elem.setAttribute("onclick", "changePic()")
+    elem.setAttribute("class", "home_img")
+    elem.src = "../static/images/beak_open.png"
+
+    var div = document.getElementById("home_beak_open")
+    if (!beakIsOpen){
+        div.appendChild(elem)
+    }
+    else{
+        div.removeChild(div.firstChild)
+    }
+    beakIsOpen = !beakIsOpen
 }
